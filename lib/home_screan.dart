324cb55055d430dc651aa6bcb022fb6785db01b9
Komariah Wulandari <komariahwulandari_2226240119@mhs.mdp.ac.id>
2024-11-02 08:56:10 +0700

    update
    menambahkan images

---
+++ lib/home_screan.dart
@@ -0,0 +1,13 @@
+class HomeScrean extends StatefulWidget {
+  const HomeScrean({super.key});
+
+  @override
+  State<HomeScrean> createState() => _HomeScreanState();
+}
+
+class _HomeScreanState extends State<HomeScrean> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
